dfe3cb28475fee90d3dde6a8795af0073ff9b3f1
Azamat <94824948+azamatnarzuloyev@users.noreply.github.com>
2024-08-06 15:37:40 +0500

    push

--- category/admin.py
+++ category/admin.py
@@ -25,3 +25,36 @@ class SubCategoryAdmin(TranslationAdmin):
         css = {
             'screen': ('modeltranslation/css/tabbed_translation_fields.css',),
         }
+
+@admin.register(MainCategory)
+class MainCategoryAdmin(TranslationAdmin):
+    formfield_overrides = {
+        JSONField: {'widget': JSONEditor},
+    }
+    group_fieldsets = False 
+    class Media:
+        js = (
+            'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
+            'http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js',
+            'modeltranslation/js/tabbed_translation_fields.js',
+        )
+        css = {
+            'screen': ('modeltranslation/css/tabbed_translation_fields.css',),
+        }
+
+
+@admin.register(SuperCategory)
+class SuperCategoryAdmin(TranslationAdmin):
+    formfield_overrides = {
+        JSONField: {'widget': JSONEditor},
+    }
+    group_fieldsets = False 
+    class Media:
+        js = (
+            'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
+            'http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js',
+            'modeltranslation/js/tabbed_translation_fields.js',
+        )
+        css = {
+            'screen': ('modeltranslation/css/tabbed_translation_fields.css',),
+        }
