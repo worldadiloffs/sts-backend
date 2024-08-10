4e7c1227e7219cf6d753119544edd85b677fbcd6
Azamat <94824948+azamatnarzuloyev@users.noreply.github.com>
2024-08-10 21:20:39 +0500

    push

--- product/urls.py
+++ product/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path 
-from .views import ProductListMiniView , CategoryProductViews , ProductPostApiView
+from .views import ProductListMiniView , CategoryProductViews 
+from .productPost import ProductPostApiView
 
 app_name = "product"
 
 urlpatterns = [
     path('sts/product/product-listt/', ProductListMiniView.as_view(), name='product-list'),
     path('sts/product/category-product/', CategoryProductViews.as_view()),
-    path('prod-post/', CategoryProductViews.as_view()),
+    path('prod-post/', ProductPostApiView.as_view()),
 ]
