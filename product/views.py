720a71592a60e4021a118b6ae60b42708923f713
Azamat <94824948+azamatnarzuloyev@users.noreply.github.com>
2024-08-15 22:13:08 +0500

    push

--- product/views.py
+++ product/views.py
@@ -100,7 +100,7 @@ class SearchProductView(APIView):
         if search:
             pass 
         next = int(request.GET.get("page", 1))
-        limit = 6
+        limit = 12
         current = int(next) - 1
         count = Product.objects.filter(status=True, site_sts=True).count()
         product = Product.objects.filter(status=True, site_sts=True).filter(Q(product_name__icontains=search)).order_by("id")[
