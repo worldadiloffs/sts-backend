3d1db38f07e38843633bc7950640f19f435ad8eb
Azamat <94824948+azamatnarzuloyev@users.noreply.github.com>
2024-08-15 23:32:31 +0500

    push

--- product/views.py
+++ product/views.py
@@ -245,11 +245,12 @@ class CategoryProductViews(APIView):
                 sub_id = SubCategory.objects.get(slug=slug).pk
                 limit = 30
                 current = int(next) - 1
+                count =  Product.objects.filter(status=True, site_sts=True, sub_category__id=sub_id)
                 product = Product.objects.filter(status=True, site_sts=True, sub_category__id=sub_id)[
                     current * limit : next * limit
                 ]
                 prod_serialzier = ProductListMiniSerilizers(product, many=True)
-                count = product.count()
+           
                 pages = int(count / limit) + 1
                 pagination = {
                     "count": count,
