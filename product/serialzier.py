18f7c0b76f162d9292820af2b139a359da9799ec
Azamat <94824948+azamatnarzuloyev@users.noreply.github.com>
2024-08-15 12:04:07 +0500

    push

--- product/serialzier.py
+++ product/serialzier.py
@@ -1,12 +1,17 @@
 from rest_framework import serializers
-from .models import Product , Image 
+from .models import Product, Image
 from config.settings import site_name
 from settings.models import OrderSetting
-from category.serializers import SuperCategoryStsMiniSerializer , MainCategortStsMiniSerializer , SubCategoryStsMiniSerialzier
+from category.serializers import (
+    SuperCategoryStsMiniSerializer,
+    MainCategortStsMiniSerializer,
+    SubCategoryStsMiniSerialzier,
+)
 
 
 class ImageSeriazilizer(serializers.ModelSerializer):
     image = serializers.SerializerMethodField()
+
     class Meta:
         model = Image
         fields = "__all__"
@@ -14,10 +19,11 @@ class ImageSeriazilizer(serializers.ModelSerializer):
     def get_image(self, obj):
         image = obj.image
         if image:
-            return site_name + image.url 
-        
+            return site_name + image.url
+
         return None
-    
+
+
 class ImagePostSeriazilizer(serializers.ModelSerializer):
     class Meta:
         model = Image
@@ -25,8 +31,9 @@ class ImagePostSeriazilizer(serializers.ModelSerializer):
 
 
 def doller_funtion():
-    order =  OrderSetting.objects.first() 
-    return order.doller * order.nds /100
+    order = OrderSetting.objects.first()
+    return order.doller * order.nds / 100
+
 
 class ProductSerialzier(serializers.ModelSerializer):
     images = ImageSeriazilizer(required=False, read_only=True, many=True)
@@ -40,12 +47,9 @@ class ProductSerialzier(serializers.ModelSerializer):
         model = Product
         fields = "__all__"
 
-
-
-
     def get_price(self, obj):
-        return obj.price and obj.price * doller_funtion() or 0 
-    
+        return obj.price and obj.price * doller_funtion() or 0
+
     # def get_super_category(self, obj):
     #     category = obj.super_category
     #     if category is not None:
@@ -54,7 +58,7 @@ class ProductSerialzier(serializers.ModelSerializer):
     #             "category_name": category.super_name,
     #             "slug": category.slug
     #         }
-        
+
     # def get_main_category(self, obj):
     #     category = obj.main_category
     #     super_category = obj.super_category
@@ -64,70 +68,101 @@ class ProductSerialzier(serializers.ModelSerializer):
     #             "main":{"id": category.id , "category_name": category.main_name, "slug": category.slug}
     #         }
 
-    
     def get_category(self, obj):
         sub_category = obj.sub_category
         super_category = obj.super_category
-        main_category = obj.main_category 
+        main_category = obj.main_category
         if sub_category is not None:
-            return {
-                {"id": super_category.id, "cateogry_name": super_category.super_name, "slug": super_category.slug, 
-                    "children":{"id": main_category.id , "category_name": main_category.main_name, "slug": main_category.slug ,
-                     "children":{"id": sub_category.id , "category_name": sub_category.sub_name, "slug": sub_category.slug}}},
+            return  {
+                    "id": super_category.id,
+                    "cateogry_name": super_category.super_name,
+                    "slug": super_category.slug,
+                    "children": {
+                        "id": main_category.id,
+                        "category_name": main_category.main_name,
+                        "slug": main_category.slug,
+                        "children": {
+                            "id": sub_category.id,
+                            "category_name": sub_category.sub_name,
+                            "slug": sub_category.slug,
+                        },
+                    },
+                },
             
-                
-            }
         if main_category is not None:
+            return  {
+                    "id": super_category.id,
+                    "name": super_category.super_name,
+                    "slug": super_category.slug,
+                    "children": {
+                        "id": main_category.id,
+                        "name": main_category.main_name,
+                        "slug": main_category.slug,
+                    },
+                },
+            
+
+        if super_category is not None:
             return {
-              {"id":super_category.id , "name": super_category.super_name, "slug": super_category.slug, 
-               "children": {"id": main_category.id , "name": main_category.main_name, "slug": main_category.slug}},  
+                "id": super_category.id,
+                "cateogry_name": super_category.super_name,
+                "slug": super_category.slug,
             }
 
-        if super_category is not None :
-            return {"id": super_category.id, "cateogry_name": super_category.super_name, "slug": super_category.slug}
-  
-        
-      
-        
+
 class ProductDetailSerialzeir(serializers.ModelSerializer):
     images = ImageSeriazilizer(required=False, read_only=True, many=True)
     super_category = serializers.SerializerMethodField()
     main_category = serializers.SerializerMethodField()
     sub_category = serializers.SerializerMethodField()
     price = serializers.SerializerMethodField()
+
     class Meta:
         model = Product
-        fields = ("id", "product_name", 'main_category','super_category', 'sub_category','images', "product_video", "slug", "price", "discount_price", "short_content","tavar_dagavornaya" , "counts", )
+        fields = (
+            "id",
+            "product_name",
+            "main_category",
+            "super_category",
+            "sub_category",
+            "images",
+            "product_video",
+            "slug",
+            "price",
+            "discount_price",
+            "short_content",
+            "tavar_dagavornaya",
+            "counts",
+        )
 
-
-    
     def get_price(self, obj):
-        return obj.price and obj.price * doller_funtion() or obj.price  
-    
+        return obj.price and obj.price * doller_funtion() or obj.price
+
     def get_super_category(self, obj):
         category = obj.super_category
         if category is not None:
             return {
                 "id": category.id,
                 "category_name": category.super_name,
-                "slug": category.slug
+                "slug": category.slug,
             }
-        
+
     def get_main_category(self, obj):
         category = obj.main_category
         if category is not None:
             return {
                 "id": category.id,
                 "category_name": category.main_name,
-                "slug": category.slug
+                "slug": category.slug,
             }
+
     def get_sub_category(self, obj):
         category = obj.sub_category
         if category is not None:
             return {
                 "id": category.id,
                 "category_name": category.sub_name,
-                "slug": category.slug
+                "slug": category.slug,
             }
 
 
@@ -136,27 +171,40 @@ class ProductListMiniSerilizers(serializers.ModelSerializer):
     image = serializers.SerializerMethodField(read_only=True)
     category_name = serializers.SerializerMethodField(read_only=True)
     price = serializers.SerializerMethodField()
+
     class Meta:
         model = Product
-        fields = ("id", "product_name",'category_name', 'image', "product_video", "slug", "price", "discount_price", "short_content","tavar_dagavornaya" , "counts", "super_category")
+        fields = (
+            "id",
+            "product_name",
+            "category_name",
+            "image",
+            "product_video",
+            "slug",
+            "price",
+            "discount_price",
+            "short_content",
+            "tavar_dagavornaya",
+            "counts",
+            "super_category",
+        )
 
     def get_price(self, obj):
-        return obj.price and obj.price * doller_funtion() or 0 
-    
+        return obj.price and obj.price * doller_funtion() or 0
+
     def get_image(self, obj):
         image = obj.image
         if image:
-            return site_name  + image
+            return site_name + image
         return None
-    
+
     def get_category_name(self, obj):
         category = obj.super_category
         if category is not None:
             return category.super_name
-    
 
 
 class ParemententCategorySerialzeir(serializers.Serializer):
     super_id = serializers.IntegerField(required=False)
-    main_id =  serializers.IntegerField(required=False)
+    main_id = serializers.IntegerField(required=False)
     sub_id = serializers.IntegerField(required=False)
