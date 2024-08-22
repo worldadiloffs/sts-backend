867b9496d08399bb0272229ebd16542680308c0c
Azamat <94824948+azamatnarzuloyev@users.noreply.github.com>
2024-08-22 16:33:28 +0500

    push

--- ordersts/order_get_validate.py
+++ ordersts/order_get_validate.py
@@ -1,5 +1,5 @@
 from rest_framework.views import APIView
-from settings.models import DeliveryService, OrderSetting, PaymentMethod , TolovUsullar , Shaharlar, Tumanlar
+from settings.models import DeliveryService, OrderSetting, PaymentMethod , TolovUsullar , Shaharlar, Tumanlar, Dokon
 from django.http import JsonResponse
 from settings.serialziers import DeliveryServiceSeriazleir, DokonSerialzier, ShaharlarSerialzier, TolovUsullarSerialzier, TumanlarSerialzier
 
@@ -8,22 +8,23 @@ from settings.serialziers import DeliveryServiceSeriazleir, DokonSerialzier, Sha
 class OrderValudeView(APIView):
     def get(self, request):
         delivery = DeliveryService.objects.all()
-        payment = PaymentMethod.objects.all()
         tolov = TolovUsullar.objects.all()
         shaharlar = Shaharlar.objects.all()
         tumanlar = Tumanlar.objects.all()
+        dokon = Dokon.objects.all()
         delivery_serial = DeliveryServiceSeriazleir(delivery, many=True)
-        payment_serial = TolovUsullarSerialzier(payment, many=True)
         tolov_serial = TolovUsullarSerialzier(tolov, many=True)
         shaharlar_serial = ShaharlarSerialzier(shaharlar, many=True)
         tumanlar_serial = TumanlarSerialzier(tumanlar, many=True)
+        dokon_serial = DokonSerialzier(dokon, many=True)
         return JsonResponse(
             {
               "data": { "delivery": delivery_serial.data,
-                "payment": payment_serial.data,
                 "tolov": tolov_serial.data,
                 "shaharlar": shaharlar_serial.data,
-                "tumanlar": tumanlar_serial.data},
+                "tumanlar": tumanlar_serial.data,
+                "dokonlar": dokon_serial.data
+                },
                 "errors": False,
                 "message": "ok"
               
