02fcf39d4c13c74687997d65670e8ba3cbcaad3d
Azamat <94824948+azamatnarzuloyev@users.noreply.github.com>
2024-08-22 16:39:52 +0500

    push

--- ordersts/order_get_validate.py
+++ ordersts/order_get_validate.py
@@ -10,19 +10,16 @@ class OrderValudeView(APIView):
         delivery = DeliveryService.objects.all()
         tolov = TolovUsullar.objects.all()
         shaharlar = Shaharlar.objects.all()
-        tumanlar = Tumanlar.objects.all()
         dokon = Dokon.objects.all()
         delivery_serial = DeliveryServiceSeriazleir(delivery, many=True)
         tolov_serial = TolovUsullarSerialzier(tolov, many=True)
         shaharlar_serial = ShaharlarSerialzier(shaharlar, many=True)
-        tumanlar_serial = TumanlarSerialzier(tumanlar, many=True)
         dokon_serial = DokonSerialzier(dokon, many=True)
         return JsonResponse(
             {
               "data": { "delivery": delivery_serial.data,
                 "tolov": tolov_serial.data,
                 "shaharlar": shaharlar_serial.data,
-                "tumanlar": tumanlar_serial.data,
                 "dokonlar": dokon_serial.data
                 },
                 "errors": False,
