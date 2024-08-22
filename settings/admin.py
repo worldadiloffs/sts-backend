e85142158080f4b5c7fb00ebb693ccdb43fbd6ef
Azamat <94824948+azamatnarzuloyev@users.noreply.github.com>
2024-08-22 16:43:24 +0500

    push

--- settings/admin.py
+++ settings/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import (CardGril , PageContent  , PaymentMethod , DeliveryService, Shaharlar ,
-                      SiteSettings , PaymentMethod , OrderSetting  , SitePage , SocialNetwork , CountSettings, Tumanlar)
+                      SiteSettings , PaymentMethod , OrderSetting  , SitePage , SocialNetwork , CountSettings, Tumanlar, TolovUsullar)
 from django.db.models.fields.json import JSONField
 from jsoneditor.forms import JSONEditor
 from modeltranslation.admin import TranslationAdmin
@@ -12,6 +12,7 @@ admin.site.register(OrderSetting)
 admin.site.register(SocialNetwork)
 admin.site.register(Shaharlar)
 admin.site.register(Tumanlar)
+admin.site.register(TolovUsullar)
 
 @admin.register(CountSettings)
 class CountSettingAdmin(admin.ModelAdmin):
