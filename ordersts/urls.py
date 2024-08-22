cec859978a72c969127904dea62e3ee517070b0e
Azamat <94824948+azamatnarzuloyev@users.noreply.github.com>
2024-08-22 13:59:50 +0500

    push

--- ordersts/urls.py
+++ ordersts/urls.py
@@ -2,3 +2,11 @@ from django.urls import path
 
 app_name = 'ordersts'
 
+from .views import OrderCreateAPIView
+# from . import views
+
+
+urlpatterns = [
+    path('sts/orders/craate/', OrderCreateAPIView.as_view(), name='order_create'),
+]
+
