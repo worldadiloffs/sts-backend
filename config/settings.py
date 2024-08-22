2447bb4a3d183d2b62b6806380489c23a4e9d886
Azamat <94824948+azamatnarzuloyev@users.noreply.github.com>
2024-08-22 13:50:07 +0500

    push

--- config/settings.py
+++ config/settings.py
@@ -357,7 +357,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=30),
     # 'ROTATE_REFRESH_TOKENS': False,
     # 'BLACKLIST_AFTER_ROTATION': True,
     # 'UPDATE_LAST_LOGIN': False,
