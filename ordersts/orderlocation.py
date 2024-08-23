8572cf0648d2be4f95ba077bcda3dba3e835cc84
Azamat <94824948+azamatnarzuloyev@users.noreply.github.com>
2024-08-23 11:19:52 +0500

    pdh

--- ordersts/orderlocation.py
+++ ordersts/orderlocation.py
@@ -1,14 +1,15 @@
 import requests
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from extensions.code_generator import get_client_ip
 
-def get_client_ip(request):
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-    if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[0]  # Birinchi IP manzil asl foydalanuvchi IPsi bo'ladi
-    else:
-        ip = request.META.get('REMOTE_ADDR')
-    return ip
+# def get_client_ip(request):
+#     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+#     if x_forwarded_for:
+#         ip = x_forwarded_for.split(',')[0]  # Birinchi IP manzil asl foydalanuvchi IPsi bo'ladi
+#     else:
+#         ip = request.META.get('REMOTE_ADDR')
+#     return ip
 
 class LocationAPIView(APIView):
     def get(self, request):
@@ -16,12 +17,13 @@ class LocationAPIView(APIView):
         # Here we use a dummy API for demonstration purposes
      # For real-world application, you'd use an external API like IP Geolocation API or MaxMind GeoIP API
         client_ip = get_client_ip(request)
-        print(client_ip)
-        client_ip = "213.230.120.73"
+        # client_ip = "213.230.120.73"
         # Replace this with your actual API request
-        api_response = requests.get(f"http://ipinfo.io/{client_ip}/geo")
+        url =f"http://ipinfo.io/{client_ip}/geo"
+
+        api_response = requests.get(url=url)
         data = api_response.json()
-        if data.get("status", {}).get("code") == 200:
+        if data:
             location_data = {
                 "city": data.get("city", ""),
                 "country": data.get("country_name", ""),
