08712aa6e3a73dde2b7e3705dd742e5698fae88f
Azamat <94824948+azamatnarzuloyev@users.noreply.github.com>
2024-08-27 15:57:48 +0500

    push

--- account/admin.py
+++ account/admin.py
@@ -6,7 +6,7 @@ from django.contrib import admin
 from .models import User, PhoneOtp , GouseUser ,  UserAddress
 
 # Register your models here.
-
+admin.site.register(UserAddress)
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
