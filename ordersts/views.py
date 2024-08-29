4205998b8b34cf6ee76ec66d5cbc2803d8d8834d
Azamat <94824948+azamatnarzuloyev@users.noreply.github.com>
2024-08-29 20:35:54 +0500

    push

--- ordersts/views.py
+++ ordersts/views.py
@@ -229,7 +229,7 @@ class OrderCreateAPIView(APIView):
         if request.data['tuman'] is not None:
             tuman = Tumanlar.objects.get(name=request.data['tuman'])
             request.data['tuman']=  tuman.pk 
-            _user_address_to_dict(shahar=request.data['shahar'], tuman=request.data['tuman'], user=request.user.id, qishloq=request.data['qishloq'])
+            _user_address_to_dict(shahar=request.data['shahar'], tuman=request.data['tuman'], user_id=request.user.id, qishloq=request.data['qishloq'])
 
 
         serializer = OrderSerializer(data=request.data)
