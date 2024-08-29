dc32c403863d781deedf4b96527ba3d0af1d7195
Azamat <94824948+azamatnarzuloyev@users.noreply.github.com>
2024-08-30 00:16:33 +0500

    push

--- ordersts/order_get_user.py
+++ ordersts/order_get_user.py
@@ -27,7 +27,7 @@ class OrderGetApiviews(APIView):
     def get(self, request):
         user = request.user
         if user.is_authenticated:
-            order = Order.objects.filter(user__id=user.id, site_sts=True).order_by("-created_at")
+            order = Order.objects.filter(user__id=user.id, order_close=True,  site_sts=True).order_by("-created_at")
             serializer = OrderGetUserSerializer(order, many=True)
             zakas_lar = []
             if serializer.data:
@@ -36,9 +36,17 @@ class OrderGetApiviews(APIView):
                         "order_items": i['order_items'],
                         "status_color": i['status_color'],
                         "order": i['order_obj'], })
-                # zakas_lar.sort(key=lambda x: x['tavarlar']['created_at'], reverse=True)
             
-            return Response({"data": zakas_lar, "errors": False, "message": ""}, status=status.HTTP_200_OK)
+            hozirgi_zakaslar = Order.objects.filter(user__id=user.id, order_close=False,  site_sts=True).order_by("-created_at")
+            hozir_serializer = OrderGetUserSerializer(hozirgi_zakaslar, many=True)
+            hozir = []
+            if hozir_serializer:
+                for i in hozir_serializer.data:
+                    hozir.append({
+                        "order_items": i['order_items'],
+                        "status_color": i['status_color'],
+                        "order": i['order_obj'], })
+            return Response({"data": {"hozir": hozir, "barchasi": zakas_lar }, "errors": False, "message": ""}, status=status.HTTP_200_OK)
         return JsonResponse({'data': None, 'errors': True, 'message': ''}, safe=False)
     
 
