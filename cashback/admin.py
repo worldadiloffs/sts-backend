2304ddb58265e4df72047d696d0afa949f9f25e7
Azamat <94824948+azamatnarzuloyev@users.noreply.github.com>
2024-08-29 18:27:23 +0500

    push

--- cashback/admin.py
+++ cashback/admin.py
@@ -10,7 +10,7 @@ from .models import CashbackKard
 
 @admin.register(CashbackKard)
 class CashbackKardAdmin(admin.ModelAdmin):
-    list_display = ('id','card','balance','created_at')
+    list_display = ('id','card','balance')
 
 
     formfield_overrides = {
