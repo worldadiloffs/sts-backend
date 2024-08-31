bb31f8b4485b0635753fb353ad7495f098a8bc3a
Azamat <94824948+azamatnarzuloyev@users.noreply.github.com>
2024-08-31 10:44:45 +0500

    push

--- home/views.py
+++ home/views.py
@@ -20,6 +20,8 @@ class BannerResponseSerialzier(serializers.Serializer):
 
 
 class BannerView(APIView):
+    @method_decorator(cache_page(60 * 60 * 2))
+    @method_decorator(vary_on_headers("Authorization"))
     @extend_schema(
             responses=BannerResponseSerialzier
     )
