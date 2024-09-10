769cfd748eec8a4bd16075948f7fbe909d902445
Azamat <94824948+azamatnarzuloyev@users.noreply.github.com>
2024-09-10 10:41:55 +0500

    push

--- importdata/productimport.py
+++ importdata/productimport.py
@@ -4,9 +4,12 @@ from .models import ImportProduct
 
 
 for i in ImportProduct.objects.all():
+    if i.quantity > 10:
+        i.quantity  = 10
     product = Product()
     product.product_name = i.name
     product.articul = i.articul
     product.price = 1
+    product.counts = i.quantity
     product.material_nomer = i.material_nomer 
     product.save()
