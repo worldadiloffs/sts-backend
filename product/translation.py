ad4af3d7784b7d7efccc04dcfae3c46a0d801843
Azamat <94824948+azamatnarzuloyev@users.noreply.github.com>
2024-09-11 13:05:51 +0500

    push

--- product/translation.py
+++ product/translation.py
@@ -4,4 +4,4 @@ from .models import Product
 
 @register(Product)
 class ProductTranslationOptions(TranslationOptions):
-    fields = ('product_name','short_content', 'meta_title', 'meta_data', 'content','full_description', 'news_title', 'aksiya_title')
+    fields = ('product_name','short_content', 'meta_title', 'meta_data', 'content','full_description', 'news_title', 'aksiya_title', 'short_description',)
