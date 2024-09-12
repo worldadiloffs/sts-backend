ed01839bcc0f2ccd0f70675b6599a9127d59fb36
Azamat <94824948+azamatnarzuloyev@users.noreply.github.com>
2024-09-12 10:25:48 +0500

    push

--- home/views.py
+++ home/views.py
@@ -20,8 +20,8 @@ class BannerResponseSerialzier(serializers.Serializer):
 
 
 class BannerView(APIView):
-    @method_decorator(cache_page(60 * 60 * 2))
-    @method_decorator(vary_on_headers("Authorization"))
+    # @method_decorator(cache_page(60 * 60 * 2))
+    # @method_decorator(vary_on_headers("Authorization"))
     @extend_schema(
             responses=BannerResponseSerialzier
     )
@@ -37,8 +37,8 @@ class BannerView(APIView):
     
 
 class BannerDetailViews(APIView):
-    @method_decorator(cache_page(60 * 60 * 2))
-    @method_decorator(vary_on_headers("Authorization"))
+    # @method_decorator(cache_page(60 * 60 * 2))
+    # @method_decorator(vary_on_headers("Authorization"))
     @extend_schema(
             responses=BannerResponseSerialzier
     )
@@ -62,8 +62,8 @@ class BannerDetailViews(APIView):
 
 
 class HomePageCategoryView(APIView):
-    @method_decorator(cache_page(60 * 60 * 2))
-    @method_decorator(vary_on_headers("Authorization"))
+    # @method_decorator(cache_page(60 * 60 * 2))
+    # @method_decorator(vary_on_headers("Authorization"))
     @extend_schema(
             responses=ResponseHOme
             )
