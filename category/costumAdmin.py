4820208182f4fc0d39945ccfe4a976fe8d4ddb2e
Azamat <94824948+azamatnarzuloyev@users.noreply.github.com>
2024-09-13 20:53:04 +0500

    push

--- category/costumAdmin.py
+++ category/costumAdmin.py
@@ -7,12 +7,12 @@ def get_main_categories(request):
     if request.method == 'GET':
         super_category_id = request.GET.get('super_category')
         main_categories = MainCategory.objects.filter(superCategory_id=super_category_id)
-        data = [{'id': cat.id, 'name': cat.name} for cat in main_categories]
+        data = [{'id': cat.id, 'name': cat.main_name} for cat in main_categories]
         return JsonResponse(data, safe=False)
 
 def get_sub_categories(request):
     if request.method == 'GET':
         main_category_id = request.GET.get('main_category')
         sub_categories = SubCategory.objects.filter(mainCategory_id=main_category_id)
-        data = [{'id': cat.id, 'name': cat.name} for cat in sub_categories]
+        data = [{'id': cat.id, 'name': cat.sub_name} for cat in sub_categories]
         return JsonResponse(data, safe=False)
