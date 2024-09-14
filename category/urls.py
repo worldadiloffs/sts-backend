beb8e478912db255b6f0d689a845d8bed1dc53d7
Azamat <94824948+azamatnarzuloyev@users.noreply.github.com>
2024-09-14 09:47:26 +0500

    push

--- category/urls.py
+++ category/urls.py
@@ -6,7 +6,9 @@ app_name = 'category'
 urlpatterns = [
     path('<str:site>/category/category-list/', CategoryListJsonViews.as_view(), name='category-list'),
     path('<str:site>/category/header/', CategoryHeaderViews.as_view()),
-    path('get_main_categories/', get_main_categories, name='get_main_categories'),
-    path('get_sub_categories/', get_sub_categories, name='get_sub_categories'),
+    path('get-main-categories/', get_main_categories, name='get_main_categories'),
+    path('get-sub-categories/', get_sub_categories, name='get_sub_categories'),
 
 ]
+
+
