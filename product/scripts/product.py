98591905ced9a70a520680897cdd0a5d97c12e3e
Azamat <94824948+azamatnarzuloyev@users.noreply.github.com>
2024-09-17 02:10:28 +0500

    push

--- product/scripts/product.py
+++ product/scripts/product.py
@@ -6,25 +6,20 @@ from product.models import Product
 
 
 
-def run(main_id=2):
-    data = []
-    for i in range(1, 13):
-        filter_super_category = MainCategory.objects.get(id=main_id).superCategory.pk
-        main_obj = MainCategory.objects.select_related('superCategory').filter(superCategory__id=filter_super_category)
-        for i in main_obj:
-            sub_category = SubCategory.objects.select_related("mainCategory").filter(mainCategory__id=i.pk)
-            if sub_category is not None:
-                for sub in sub_category:
-                    prod_count = Product.objects.select_related("sub_category").filter(sub_category__id=sub.pk).count()
-                    data.append(
-                        {
-                            "sub_name": sub.sub_name,
-                            "counts": prod_count,
-                            "slug": sub.slug,
-                            "pk": sub.pk,
-                        }
-                    )
-                    if len(data) > 12:
-                        return data
+def run():
+    product = Product.objects.filter(site_sts=True)
+    image_count = 0
+    content = 0
+    category_count = 0
+    for i in product:
+        if i.short_description is not None:
+            content += 1
+        if i.image is not None:
+            image_count += 1
+        if i.super_category is not None and i.main_category is not None:
+            category_count +=1
+
+
+
             
 
