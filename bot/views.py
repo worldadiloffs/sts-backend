a79ef35611cddb0e398434702088f7a6b80b7b65
Azamat <94824948+azamatnarzuloyev@users.noreply.github.com>
2024-09-21 12:10:17 +0500

    dsds

--- bot/views.py
+++ bot/views.py
@@ -15,12 +15,12 @@ def attendance(request):
     latitude = request.data.get('latitude')  # Lokatsiya kengligi
     longitude = request.data.get('longitude')  # Lokatsiya uzunligi
     now = timezone.now()
-    time = now.strptime('%H:%M')
+    time_str = now.strftime('%H:%M')
     # time_str = time 
-    now = timezone.now()
-    hours = str(now.hour)
-    minut = str(now.minute)
-    time_str = f"{time}"
+    # now = timezone.now()
+    # hours = str(now.hour)
+    # minut = str(now.minute)
+    # time_str = f"{time}"
     if action == 'register':
         name = request.data.get('name')
         phone_number = request.data.get('phone_number')
