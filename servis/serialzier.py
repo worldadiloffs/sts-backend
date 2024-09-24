35c56037eca9d18ea0e22a7975a4e7d326985bb9
Azamat <94824948+azamatnarzuloyev@users.noreply.github.com>
2024-09-24 11:36:46 +0500

    dsd

--- servis/serialzier.py
+++ servis/serialzier.py
@@ -13,10 +13,10 @@ class JopServisCardSerializer(serializers.ModelSerializer):
 #     status = models.BooleanField(default=False, blank=True)
 
 class JopServisSerializer(serializers.ModelSerializer):
-    jopserviscards = JopServisCardSerializer(required=False, read_only=True, many=True)
+    jopserviscard = JopServisCardSerializer(required=False, read_only=True, many=True)
     class Meta:
         model = JopServis
-        fields = ("header_title", "header_title_text", "bground_image", "status", "jopserviscards",)
+        fields = ("header_title", "header_title_text", "bground_image", "status", "jopserviscard",)
 
 
 
