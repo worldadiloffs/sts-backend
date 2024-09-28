d8cf02a1a4efcb39e7012438273bce32931a915b
Azamat <94824948+azamatnarzuloyev@users.noreply.github.com>
2024-09-28 22:51:44 +0500

    dsdsd

--- product/serialzier.py
+++ product/serialzier.py
@@ -240,8 +240,10 @@ class ProductListMiniSerilizers(serializers.ModelSerializer):
 
 
     def get_cashback_value(self, obj):
-        cash = cashback_values(products=[{"id": obj.id, "count": 1}])
-        return int(cash["data"])
+        if obj.id is not None:
+            cash = cashback_values(products=[{"id": obj.id, "count": 1}])
+            return int(cash["data"])
+        return None
 
 
     def get_kredit_summa(self, obj):
