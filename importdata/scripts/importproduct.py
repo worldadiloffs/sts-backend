0fb1365b63a44a2f0a08a9485b739c267d284122
Azamat <94824948+azamatnarzuloyev@users.noreply.github.com>
2024-10-03 12:49:37 +0500

    sasssa

--- importdata/scripts/importproduct.py
+++ importdata/scripts/importproduct.py
@@ -2,17 +2,23 @@ from product.models import Product
 from importdata.models import ImportProduct
 
 
+# def run():
+#     for i in ImportProduct.objects.all()[1875:]:
+#         if i.name is not None:
+#             materila_bool = Product.objects.filter(material_nomer=i.material_nomer).exists()
+#             articul_bool = Product.objects.filter(articul=i.articul).exists()
+#             if not(materila_bool) and not(articul_bool):
+#                 product = Product()
+#                 product.product_name = i.name
+#                 product.articul = i.articul
+#                 product.price = 1
+#                 product.counts = i.quantity
+#                 product.material_nomer = i.material_nomer 
+#                 product.site_rts = True  # site status true
+#                 product.save()
+
+
 def run():
-    for i in ImportProduct.objects.all()[1875:]:
-        if i.name is not None:
-            materila_bool = Product.objects.filter(material_nomer=i.material_nomer).exists()
-            articul_bool = Product.objects.filter(articul=i.articul).exists()
-            if not(materila_bool) and not(articul_bool):
-                product = Product()
-                product.product_name = i.name
-                product.articul = i.articul
-                product.price = 1
-                product.counts = i.quantity
-                product.material_nomer = i.material_nomer 
-                product.site_rts = True  # site status true
-                product.save()
+    impor_prod = ImportProduct.objects.all()
+    impor_prod.delete()
+    
