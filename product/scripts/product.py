baed278d9a73fb56102a83d0cb6fd850b39a5550
Azamat <94824948+azamatnarzuloyev@users.noreply.github.com>
2024-10-19 10:53:53 +0500

    fdf

--- product/scripts/product.py
+++ product/scripts/product.py
@@ -8,19 +8,25 @@ from product.models import Product
 
 def run():
     product = Product.objects.filter(site_sts=True)
-    image_count = 0
-    content = 0
-    category_count = 0
+
     for i in product:
-        if i.short_description is not None:
-            content += 1
-        if i.image is not None:
-            image_count += 1
-        if i.super_category is not None and i.main_category is not None:
-            category_count +=1
-    print("image count: ", image_count)
-    print("content count: ", content)
-    print("category count: ", category_count)
+        if int(i.price) == 1:
+            i.status = False
+            i.save()
+            
+    # image_count = 0
+    # content = 0
+    # category_count = 0
+    # for i in product:
+    #     if i.short_description is not None:
+    #         content += 1
+    #     if i.image is not None:
+    #         image_count += 1
+    #     if i.super_category is not None and i.main_category is not None:
+    #         category_count +=1
+    # print("image count: ", image_count)
+    # print("content count: ", content)
+    # print("category count: ", category_count)
 
     # prod = Product.objects.filter(site_rts=True,price=1).count()
     # print(prod)
