7da2b8956cd97998acebd561787cdb384a5fe384
Azamat <94824948+azamatnarzuloyev@users.noreply.github.com>
2024-10-19 11:26:01 +0500

    dsdsd

--- product/content_process.py
+++ product/content_process.py
@@ -3,7 +3,8 @@ from .models import Product
 
 def content_product(request):
     product_status_true = Product.objects.filter(status=True, site_sts=True).count()
-    # product_status_false = Product.objects.filter(status=False, site_sts=True).count()
+    product_rts_status_false = Product.objects.filter(status=True, site_rts=True).count()
+    rts__tavarlar = Product.objects.filter(site_rts=True,).count()
     product_count_all = Product.objects.filter(site_sts=True).count()
 
-    return {"product_status_true": product_status_true,  "product_count_all": product_count_all  }
+    return {"product_status_true": product_status_true,  "product_count_all": product_count_all , "rts_tavarlar_active": product_rts_status_false , "rts_tavarlar":rts__tavarlar}
