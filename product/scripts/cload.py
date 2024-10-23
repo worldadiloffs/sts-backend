27b2ff2e2f7d2879f3f20135e29ca433663c5414
Azamat <94824948+azamatnarzuloyev@users.noreply.github.com>
2024-10-23 23:27:53 +0500

    fdfdf

--- product/scripts/cload.py
+++ product/scripts/cload.py
@@ -16,7 +16,7 @@ from product.models import Image
 def run():
     # count = Image.objects.all().count()
     # print(count)
-    image = Image.objects.all()[1699:1800]
+    image = Image.objects.all()[1799:]
     j = 0
     for i in image:
         if i.image is not None:
