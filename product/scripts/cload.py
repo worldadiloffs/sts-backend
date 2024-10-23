73111fdde97f2cedb5cadc4357abc63c6f66f288
Azamat <94824948+azamatnarzuloyev@users.noreply.github.com>
2024-10-23 22:10:39 +0500

    fdff

--- product/scripts/cload.py
+++ product/scripts/cload.py
@@ -1,13 +1,24 @@
 from home.models import Banner
 from product.servisses import upload_image_to_cloudflare
+from product.models import Image
+
+
+# def run():
+#     homes = Banner.objects.all()
+#     for home in homes:
+#         if home.image:
+#             cload_id = upload_image_to_cloudflare(home.image.file)
+#             home.cloudflare_id = cload_id
+#             home.save()
+#             print(home.cloudflare_id)
 
 
 def run():
-    homes = Banner.objects.all()
-    for home in homes:
-        if home.image:
-            cload_id = upload_image_to_cloudflare(home.image.file)
-            home.cloudflare_id = cload_id
-            home.save()
-            print(home.cloudflare_id)
+    image = Image.objects.all()[:100]
+    for i in image:
+        if i.image is not None:
+            cload_id = upload_image_to_cloudflare(i.image.file)
+            i.cloudflare_id = cload_id
+            i.save()
+            print(i.cloudflare_id)
 
