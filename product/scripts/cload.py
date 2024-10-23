374a2fae9a8852d0648100fc1b3277a31d195a23
Azamat <94824948+azamatnarzuloyev@users.noreply.github.com>
2024-10-23 22:24:21 +0500

    fdfdf

--- product/scripts/cload.py
+++ product/scripts/cload.py
@@ -14,7 +14,7 @@ from product.models import Image
 
 
 def run():
-    image = Image.objects.all()[199:300]
+    image = Image.objects.all()[299:400]
     for i in image:
         if i.image is not None:
             cload_id = upload_image_to_cloudflare(i.image.file)
