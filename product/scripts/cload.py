fcbe9340be23f901637a86953a3ef0cca561bee6
Azamat <94824948+azamatnarzuloyev@users.noreply.github.com>
2024-10-23 21:51:56 +0500

    Fdfdf

--- product/scripts/cload.py
+++ product/scripts/cload.py
@@ -5,8 +5,9 @@ from product.servisses import upload_image_to_cloudflare
 def run():
     homes = Banner.objects.all()
     for home in homes:
-        cload_id = upload_image_to_cloudflare(home.image.file)
-        home.cloudflare_id = cload_id
-        home.save()
-        print(home.cloudflare_id)
+        if home.image:
+            cload_id = upload_image_to_cloudflare(home.image.file)
+            home.cloudflare_id = cload_id
+            home.save()
+            print(home.cloudflare_id)
 
