86077f371e29e283afc76cc913934cbd584e4bc1
Azamat <94824948+azamatnarzuloyev@users.noreply.github.com>
2024-11-03 13:14:40 +0500

    push

--- product/scripts/crmprod.py
+++ product/scripts/crmprod.py
@@ -26,7 +26,7 @@ def save_meth():
 
 
 def run():
-    product = Product.objects.filter(site_sts=True)[500:700]
+    product = Product.objects.filter(site_sts=True)[700:1000]
     for i in product:
         if i.material_nomer is not None:
             serena = True
