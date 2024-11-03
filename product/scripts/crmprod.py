fa680ba77aaad6dc532368031a12f19d37a4d8fc
Azamat <94824948+azamatnarzuloyev@users.noreply.github.com>
2024-11-03 13:02:14 +0500

    push

--- product/scripts/crmprod.py
+++ product/scripts/crmprod.py
@@ -1,5 +1,27 @@
 import requests
 from product.models import Product
+from random import randint, randrange, Random, random
+import string
+
+string.ascii_letters
+"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+import random
+
+random.choice(string.ascii_letters)
+
+
+def random_char(y):
+    return "".join(random.choice(string.ascii_letters) for x in range(y))
+
+
+# print (random_char(5))
+
+
+# print(randint(10000000, 99999999))
+def save_meth():
+    data = []
+    for i in range(50):
+        data.append(f"{random_char(2)}{randint(100000, 200000)}")
 
 
 def run():
@@ -7,8 +29,13 @@ def run():
     for i in product:
         if i.material_nomer is not None:
             serena = True
+            product_serena = save_meth()
+            product_count = None
+
         else:
             serena = False
+            product_serena = None
+            product_count = randint(1, 100)
         req = {
             "product_name": i.product_name,
             "articul": i.articul,
@@ -17,6 +44,8 @@ def run():
             "material_nomer": i.material_nomer,
             "serenaTrue_countFalse": serena,
             "cloud_id": i.cloud_id,
+            "product_count": product_count,
+            "product_serena": product_serena,
         }
         response = requests.post('https://hikvision-shop.uz/crm/product-data/', json=req)
         if response.status_code == 200:
