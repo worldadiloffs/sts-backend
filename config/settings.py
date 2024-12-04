90abbc209951731f492974a0bcbfb67eb5d2b3e5
Azamat <94824948+azamatnarzuloyev@users.noreply.github.com>
2024-12-04 22:18:01 +0500

    push

--- config/settings.py
+++ config/settings.py
@@ -91,6 +91,7 @@ SILKY_MIDDLEWARE_CLASS = "silk.middleware.SilkyMiddleware"
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://127.0.0.1:3001",
+    'http://localhost:5173/',
 ]
 
 MIDDLEWARE = [
