1445575148c5189291c171f49cee9b7ef63d1b4e
Azamat <94824948+azamatnarzuloyev@users.noreply.github.com>
2024-12-04 22:26:06 +0500

    push

--- config/settings.py
+++ config/settings.py
@@ -93,6 +93,14 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3001",
     'http://localhost:5173/',
 ]
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
 
 MIDDLEWARE = [
     # "debug_toolbar.middleware.DebugToolbarMiddleware",
