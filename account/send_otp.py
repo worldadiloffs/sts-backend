8658be08fd20cb40d12b16b9ad6e9bca1a37ebdc
Azamat <94824948+azamatnarzuloyev@users.noreply.github.com>
2024-12-09 10:37:40 +0500

    push

--- account/send_otp.py
+++ account/send_otp.py
@@ -4,6 +4,37 @@ from extensions.code_generator import otp_generator, get_client_ip
 from rest_framework import status
 from rest_framework.response import Response
 from django.conf import settings
+import json
+
+
+
+def eskiz_token():
+    import requests
+    req = requests.post('https://notify.eskiz.uz/api/auth/login', data = {'email': 'azamatsabina1796@mail.ru', 'password': 'AAoWE9juaZ4CirQv2Oyr06yVrloyX5o7oVgZ6Zu8'})
+    response = req.json()
+    token = response['data']['token']
+    json_data = {
+        "SMS_TOKEN": token
+    }
+
+    with open("settings.json", "w") as file:
+        json.dump(json_data, file, indent=4)
+    
+
+def eskiz_token_read():
+    with open('settings.json') as f:
+        data = json.load(f)
+        SMS_TOKEN = data['SMS_TOKEN']
+        return SMS_TOKEN
+    
+
+def send_sms_otp(phone, otp):
+    sms_token = eskiz_token_read()
+    url = f'http://notify.eskiz.uz/api/message/sms/send?mobile_phone={phone}&from=4546&message=sts-hik.uz kirish uchun parol:{otp} ) '
+    headers = {'Authorization': f'Bearer {sms_token}'}
+    response = requests.post(url, headers=headers)
+    return response.status_code
+        
 
 
 # send otp code 
@@ -16,29 +47,36 @@ def send_otp(request, phone):
     cache.set(phone, otp, settings.EXPIRY_TIME_OTP)
     # send sms code
 
-    url = f'http://notify.eskiz.uz/api/message/sms/send?mobile_phone={phone}&from=4546&message=sts-hik.uz kirish uchun parol:{otp} ) '
-    headers = {'Authorization': f'Bearer {settings.SMS_TOKEN}'}
-
-
-    response = requests.post(url, headers=headers)
-    # if response.status_code == 401:
-
-
-    # if not(response.status_code == 200):
-
-
-
-    data_set = {
-        "status": response.status_code,
-        # "otp": otp,
-        "errors": False,
-        "message": "send",
-        # "sms_provayder": response.json()
-
-    }
-    return Response(
-        data_set,
-        status=status.HTTP_200_OK,
-    )
-
+    response = send_sms_otp(phone, otp)
+    if int(response) == 200:
+        print("token ishlayapti")
+        return Response(
+            {
+                "status": response,
+                "errors": False,
+                "message": "success",
+            },
+            status=status.HTTP_200_OK,
+        )
+    else:
+        token_update = eskiz_token()
+        print("token update bo'ldi ")
+        response = send_sms_otp(phone, otp)
+        if int(response) == 200:
+            return Response(
+                {
+                    "status": response,
+                    "errors": True,
+                    "message": "success",
+                },
+                status=status.HTTP_200_OK,
+            )
+        return Response(
+            {
+                "status": response,
+                "errors": True,
+                "message": "error",
+            },
+            status=status.HTTP_400_BAD_REQUEST,
+        )    
 
